9cf1aba709f0e8cd0cea3ad5e8e6f1b2e9543ed7
Paul N Barnes <paulieb99@gmail.com>
2021-08-03 16:49:04 -0700

    created a post route for new blog posts within controller folder

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 const { Post } = require("../../models");
 const authorize = require("../../utils/authorize");
 
-router.post("/", authorize);
+router.post("/", authorize, async (req, res) => {
+  try {
+    const newBlogPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newBlogPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
