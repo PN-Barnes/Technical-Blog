8e4ab125af22a5ce31b044e816913fd8c1a1f681
Paul N Barnes <paulieb99@gmail.com>
2021-08-03 16:46:08 -0700

    added authorization helper to check log in status

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
+const authorize = require("../../utils/authorize");
+
+router.post("/", authorize);
 
 module.exports = router;
