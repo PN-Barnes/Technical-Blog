466d480f8ca7c8394f97d8a867f60ec4f97f974a
Paul N Barnes <paulieb99@gmail.com>
2021-08-02 19:45:45 -0700

    reformatted dashboard routes file

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,2 +1,7 @@
 const router = require("express").Router();
 const { Post, User } = require("../models");
+const { post } = require("./api/userRoutes");
+
+router.get("/", async (req, res) => {
+  const posts = await post.findAll({});
+});
