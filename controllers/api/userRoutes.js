d56232656d88e487147dd611f67c3f0ac540c1f8
Paul N Barnes <paulieb99@gmail.com>
2021-08-02 19:23:47 -0700

    worked on user routes for login and login, also formatted the code with prettier

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post("/");
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again." });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again." });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userdate, message: "You are loggedn in" });
+    });
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+router.post("/logout", async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch {}
+});
+
+module.exports = router;
